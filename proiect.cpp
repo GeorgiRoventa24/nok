b65a598f818c87dfaf1679bc743d36f386bba239
GeorgiRoventa24 <ana.roventa@student.upt.ro>
2018-05-17 23:53:17 +0300

    modified class Watcher(add set and get)

--- proiect.cpp
+++ proiect.cpp
@@ -39,7 +39,35 @@ class Watcher
 		list<StateMachine<T>> stateMachinesPtr_;
 		U messsage_;
 		Z relevance_;
-		
+	public:
+	    Watcher(U m, Z r)
+	    {
+	        this.messsage_ = m;
+	        this.relevance_ = r;
+	    }
+	    
+	    void setMessage(U messsage_)
+	    {
+	        this.messsage_ = messsage_;
+	        
+	    }
+	    
+	    U getMessage()
+	    {
+	        return this.messsage_;
+	    }
+	    
+	    void setRelevance(Z relevance_)
+	    {
+	        this.relevance_ = relevance_;
+	    }
+	    
+	    Z getRelevance()
+	    {
+	        return this.relevance_;
+	    }
+	    
+	    // o functie ce comunica cu StateMachine iar cand se modifica starea StateMachine, Watcher instinteaza pe Notify
 };
 
 template<class T, class U, class Z>
@@ -73,6 +101,21 @@ shared_ptr<const Notifier<T,U,Z>> ncPtr_;
 
 int main(){
    
+    SM sm1, sm2, sm3;
+    StateMachine<int> stateMachineContainer;
+    sm1.ipRangeStart = "20";
+    sm1.ipRangeStop = "40";
+    sm1.clients = 2;
+    sm2.ipRangeStart = "40";
+    sm2.ipRangeStop = "60";
+    sm2.clients = 4;
+    sm3.ipRangeStart = "60";
+    sm3.ipRangeStop = "80";
+    sm3.clients = 10;
+    stateMachineContainer.addInContainer(sm1);
+    stateMachineContainer.addInContainer(sm2);
+    stateMachineContainer.addInContainer(sm3);
+    stateMachineContainer.printContainer();
     
     return 0;
 }
